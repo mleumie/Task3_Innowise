9f27179a4f836e056a6881688c4029e17d431cee
Aliaksei <Laptanto@gmail.com>
2026-05-25 16:37:01 +0300

    Delete src/main directory

--- src/main/java/org/laptanovich/multithreading/state/ShipState.java
+++
@@ -1,8 +0,0 @@
-package org.laptanovich.multithreading.state;
-
-import org.laptanovich.multithreading.entity.Ship;
-import org.laptanovich.multithreading.exception.CustomException;
-
-public interface ShipState {
-    void next(Ship ship) throws CustomException;
-}
